f15a3aa02ed96b975d2f01b7c09f589520169ef1
leandro-mancini <mancinilele@gmail.com>
2024-09-21 23:10:58 -0300

    feat(sandbox): update example"

--- apps/react/src/app/app.tsx
+++ apps/react/src/app/app.tsx
@@ -1,8 +1,16 @@
+import { Box, DittoProvider, defaultConfig } from 'packages/react/src/index'
+import { createSystem } from 'packages/styled-system/src/index'
+
+export const system = createSystem(defaultConfig)
+
+console.log('system', system)
+
 export function App() {
   return (
-    <div>
+    <DittoProvider value={system}>
       <h1>Sandbox React</h1>
-    </div>
+      <Box bgColor={'tomato'} padding={'24px'} color={'white'}>Box</Box>
+    </DittoProvider>
   );
 }
 
