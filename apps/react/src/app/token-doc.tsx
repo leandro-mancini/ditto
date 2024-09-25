df76b182e2503a67ae5cc60993200bfd1749b5af
leandro-mancini <mancinilele@gmail.com>
2024-09-24 21:08:32 -0300

    feat: 🎸 update token doc

--- apps/react/src/app/token-doc.tsx
+++ apps/react/src/app/token-doc.tsx
@@ -7,7 +7,13 @@ interface TokenDocProps extends BoxProps {
 export const TokenDoc = (props: TokenDocProps) => {
   const { title, children, action, ...rest } = props;
   return (
-    <Box bg="bg" rounded="0.75rem" borderWidth="0.5px" {...rest}>
+    <Box
+      bg="bg"
+      rounded="0.75rem"
+      borderWidth="0.5px"
+      borderColor="theme1.border.c"
+      {...rest}
+    >
       <Box p="1.5rem" pb="0">
         {title && <Box as="h2">{title}</Box>}
         {action}
