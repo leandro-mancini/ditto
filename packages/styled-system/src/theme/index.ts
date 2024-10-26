7432edc0e484424539ed8b4cb707f9269d7ae95c
leandro-mancini <mancinilele@gmail.com>
2024-10-25 23:19:42 -0300

    feat: 🎸 update styled-system

--- packages/styled-system/src/theme/index.ts
+++ packages/styled-system/src/theme/index.ts
@@ -4,7 +4,12 @@ import { globalCss } from './global-css';
 import { keyframes } from './keyframes';
 import { animationStyles } from './motion-styles';
 import { semanticColors } from './semantic-tokens/colors';
+import { animations } from './tokens/animations';
+import { aspectRatios } from './tokens/aspect-ratios';
 import { colors } from './tokens/colors';
+import { durations } from './tokens/durations';
+import { easings } from './tokens/easings';
+import { zIndices } from './tokens/z-indices';
 
 export const defaultThemeConfig = defineConfig({
   preflight: true,
@@ -15,7 +20,12 @@ export const defaultThemeConfig = defineConfig({
     breakpoints: breakpoints,
     keyframes: keyframes,
     tokens: {
+      animations,
+      aspectRatios,
       colors,
+      durations,
+      easings,
+      zIndex: zIndices,
     },
     semanticTokens: {
       colors: semanticColors,
