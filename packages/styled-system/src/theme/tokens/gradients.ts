b0a8dc8def3495db2c92b3b3bd78043418896f54
leandro-mancini <mancinilele@gmail.com>
2024-11-10 18:56:07 -0300

    feat: 🎸 create token gradient

---
+++ packages/styled-system/src/theme/tokens/gradients.ts
@@ -0,0 +1,52 @@
+import { defineTokens } from '../../core';
+
+export const gradients = defineTokens.gradients({
+  linear: {
+    2: {
+      '01': { value: 'linear-gradient(to right, #FF9A9E, #FAD0C4)' },
+      '02': { value: 'linear-gradient(to right, #A18CD1, #FBC2EB)' },
+      '03': { value: 'linear-gradient(to right, #FAD0C4, #FFD1FF)' },
+      '04': { value: 'linear-gradient(to right, #1A2980, #26D0CE)' },
+      '05': { value: 'linear-gradient(to right, #FF9A9E, #FAD0C4)' },
+      '06': { value: 'linear-gradient(to right, #FF9A9E, #FAD0C4)' },
+      '07': { value: 'linear-gradient(to right, #FBC5EC, #89B1F7)' },
+      '08': { value: 'linear-gradient(to right, #FF0844, #FFB199)' },
+      '09': { value: 'linear-gradient(to right, #7028E4, #E5B2CA)' },
+      '10': { value: 'linear-gradient(to right, #FFC796, #FF6B95)' },
+      '11': { value: 'linear-gradient(to right, #6654F1, #69EACB)' },
+      '12': { value: 'linear-gradient(to right, #6B8CCE, #0C3483)' },
+      '13': { value: 'linear-gradient(to right, #50C9C3, #96DEDA)' },
+      '14': { value: 'linear-gradient(to right, #6457C6, #C0ADFF)' },
+      '15': { value: 'linear-gradient(to right, #FAACA8, #DDD6F3)' },
+      '16': { value: 'linear-gradient(to right, #EACDA3, #E6B980)' },
+      '17': { value: 'linear-gradient(to right, #005BEA, #45D8FF)' },
+      '18': { value: 'linear-gradient(to right, #D4FC79, #96E6A1)' },
+      '19': { value: 'linear-gradient(to right, #FF758C, #FFA1C8)' },
+      '20': { value: 'linear-gradient(to right, #FF9A9E, #8DDAD5)' },
+      '21': { value: 'linear-gradient(to right, #B1AFF0, #836DF0)' },
+      '22': { value: 'linear-gradient(to right, #71DDA6, #70B2BC)' },
+    },
+    3: {
+      '01': { value: 'linear-gradient(to right, #1FA2FF, #12D8FA, #A6FFCB)' },
+      '02': { value: 'linear-gradient(to right, #FFBAC3, #C5C1FF, #2CD8D5)' },
+      '03': { value: 'linear-gradient(to right, #3A1C71, #D76D77, #FFCA7B)' },
+      '04': { value: 'linear-gradient(to right, #6654F1, #EACCF8, #69EACB)' },
+      '05': { value: 'linear-gradient(to right, #4801FF, #7918F2, #AC32E4)' },
+      '06': { value: 'linear-gradient(to right, #9CECFB, #65C7F7, #0A40FF)' },
+      '07': { value: 'linear-gradient(to right, #F7F0AC, #ACF7F0, #F0ACF7)' },
+      '08': { value: 'linear-gradient(to right, #C6FFDD, #FBD786, #F7797D)' },
+      '09': { value: 'linear-gradient(to right, #3B41C5, #A981BB, #FFC8A9)' },
+    },
+    4: {
+      '01': {
+        value: 'linear-gradient(to right, #35EB93, #2CACD1, #2B76B9, #3D3393)',
+      },
+    },
+    8: {
+      '01': {
+        value:
+          'linear-gradient(to right, #3F51B1, #5A55AE, #7B5FAC, #A86AA4, #CC6B8E, #F18271, #F3A469, #F7C978)',
+      },
+    },
+  },
+});
