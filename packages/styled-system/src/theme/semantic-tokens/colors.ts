3daa97bee13bdabe054f154fc7934ead36b13716
leandro-mancini <mancinilele@gmail.com>
2024-11-11 16:43:26 -0300

    refactor: 💡 update tokens colors

--- packages/styled-system/src/theme/semantic-tokens/colors.ts
+++ packages/styled-system/src/theme/semantic-tokens/colors.ts
@@ -2,96 +2,198 @@ import { defineSemanticTokens } from '../../core';
 
 export const semanticColors = defineSemanticTokens.colors({
   primary: {
-    25: { value: { _light: '{colors.blue.25}', _dark: '{colors.blue.25}' } },
-    50: { value: { _light: '{colors.blue.50}', _dark: '{colors.blue.50}' } },
-    100: { value: { _light: '{colors.blue.100}', _dark: '{colors.blue.100}' } },
-    200: { value: { _light: '{colors.blue.200}', _dark: '{colors.blue.200}' } },
-    300: { value: { _light: '{colors.blue.300}', _dark: '{colors.blue.300}' } },
-    400: { value: { _light: '{colors.blue.400}', _dark: '{colors.blue.400}' } },
-    500: { value: { _light: '{colors.blue.500}', _dark: '{colors.blue.500}' } },
-    600: { value: { _light: '{colors.blue.600}', _dark: '{colors.blue.600}' } },
-    700: { value: { _light: '{colors.blue.700}', _dark: '{colors.blue.700}' } },
-    800: { value: { _light: '{colors.blue.800}', _dark: '{colors.blue.800}' } },
-    900: { value: { _light: '{colors.blue.900}', _dark: '{colors.blue.900}' } },
-  },
-  success: {
-    25: { value: { _light: '{colors.green.25}', _dark: '{colors.green.25}' } },
-    50: { value: { _light: '{colors.green.50}', _dark: '{colors.green.50}' } },
+    25: {
+      value: { _light: '{colors.blue.25}', _dark: '{colors.darkblue.25}' },
+    },
+    50: {
+      value: { _light: '{colors.blue.50}', _dark: '{colors.darkblue.50}' },
+    },
     100: {
-      value: { _light: '{colors.green.100}', _dark: '{colors.green.100}' },
+      value: { _light: '{colors.blue.100}', _dark: '{colors.darkblue.100}' },
     },
     200: {
-      value: { _light: '{colors.green.200}', _dark: '{colors.green.200}' },
+      value: { _light: '{colors.blue.200}', _dark: '{colors.darkblue.200}' },
     },
     300: {
-      value: { _light: '{colors.green.300}', _dark: '{colors.green.300}' },
+      value: { _light: '{colors.blue.300}', _dark: '{colors.darkblue.300}' },
     },
     400: {
-      value: { _light: '{colors.green.400}', _dark: '{colors.green.400}' },
+      value: { _light: '{colors.blue.400}', _dark: '{colors.darkblue.400}' },
     },
     500: {
-      value: { _light: '{colors.green.500}', _dark: '{colors.green.500}' },
+      value: { _light: '{colors.blue.500}', _dark: '{colors.darkblue.500}' },
     },
     600: {
-      value: { _light: '{colors.green.600}', _dark: '{colors.green.600}' },
+      value: { _light: '{colors.blue.600}', _dark: '{colors.darkblue.600}' },
     },
     700: {
-      value: { _light: '{colors.green.700}', _dark: '{colors.green.700}' },
+      value: { _light: '{colors.blue.700}', _dark: '{colors.darkblue.700}' },
     },
     800: {
-      value: { _light: '{colors.green.800}', _dark: '{colors.green.800}' },
+      value: { _light: '{colors.blue.800}', _dark: '{colors.darkblue.800}' },
     },
     900: {
-      value: { _light: '{colors.green.900}', _dark: '{colors.green.900}' },
+      value: { _light: '{colors.blue.900}', _dark: '{colors.darkblue.900}' },
+    },
+  },
+  neutral: {
+    25: { value: { _light: '{colors.slate.25}', _dark: '{colors.slate.25}' } },
+    50: { value: { _light: '{colors.slate.50}', _dark: '{colors.slate.50}' } },
+    100: {
+      value: { _light: '{colors.slate.100}', _dark: '{colors.slate.100}' },
+    },
+    200: {
+      value: { _light: '{colors.slate.200}', _dark: '{colors.slate.200}' },
+    },
+    300: {
+      value: { _light: '{colors.slate.300}', _dark: '{colors.slate.300}' },
+    },
+    400: {
+      value: { _light: '{colors.slate.400}', _dark: '{colors.slate.400}' },
+    },
+    500: {
+      value: { _light: '{colors.slate.500}', _dark: '{colors.slate.500}' },
+    },
+    600: {
+      value: { _light: '{colors.slate.600}', _dark: '{colors.slate.600}' },
+    },
+    700: {
+      value: { _light: '{colors.slate.700}', _dark: '{colors.slate.700}' },
+    },
+    800: {
+      value: { _light: '{colors.slate.800}', _dark: '{colors.slate.800}' },
+    },
+    900: {
+      value: { _light: '{colors.slate.900}', _dark: '{colors.slate.900}' },
+    },
+  },
+  success: {
+    25: {
+      value: { _light: '{colors.green.25}', _dark: '{colors.darkgreen.25}' },
+    },
+    50: {
+      value: { _light: '{colors.green.50}', _dark: '{colors.darkgreen.50}' },
+    },
+    100: {
+      value: { _light: '{colors.green.100}', _dark: '{colors.darkgreen.100}' },
+    },
+    200: {
+      value: { _light: '{colors.green.200}', _dark: '{colors.darkgreen.200}' },
+    },
+    300: {
+      value: { _light: '{colors.green.300}', _dark: '{colors.darkgreen.300}' },
+    },
+    400: {
+      value: { _light: '{colors.green.400}', _dark: '{colors.darkgreen.400}' },
+    },
+    500: {
+      value: { _light: '{colors.green.500}', _dark: '{colors.darkgreen.500}' },
+    },
+    600: {
+      value: { _light: '{colors.green.600}', _dark: '{colors.darkgreen.600}' },
+    },
+    700: {
+      value: { _light: '{colors.green.700}', _dark: '{colors.darkgreen.700}' },
+    },
+    800: {
+      value: { _light: '{colors.green.800}', _dark: '{colors.darkgreen.800}' },
+    },
+    900: {
+      value: { _light: '{colors.green.900}', _dark: '{colors.darkgreen.900}' },
     },
   },
   warning: {
     25: {
-      value: { _light: '{colors.yellow.25}', _dark: '{colors.yellow.25}' },
+      value: { _light: '{colors.yellow.25}', _dark: '{colors.darkyellow.25}' },
     },
     50: {
-      value: { _light: '{colors.yellow.50}', _dark: '{colors.yellow.50}' },
+      value: { _light: '{colors.yellow.50}', _dark: '{colors.darkyellow.50}' },
     },
     100: {
-      value: { _light: '{colors.yellow.100}', _dark: '{colors.yellow.100}' },
+      value: {
+        _light: '{colors.yellow.100}',
+        _dark: '{colors.darkyellow.100}',
+      },
     },
     200: {
-      value: { _light: '{colors.yellow.200}', _dark: '{colors.yellow.200}' },
+      value: {
+        _light: '{colors.yellow.200}',
+        _dark: '{colors.darkyellow.200}',
+      },
     },
     300: {
-      value: { _light: '{colors.yellow.300}', _dark: '{colors.yellow.300}' },
+      value: {
+        _light: '{colors.yellow.300}',
+        _dark: '{colors.darkyellow.300}',
+      },
     },
     400: {
-      value: { _light: '{colors.yellow.400}', _dark: '{colors.yellow.400}' },
+      value: {
+        _light: '{colors.yellow.400}',
+        _dark: '{colors.darkyellow.400}',
+      },
     },
     500: {
-      value: { _light: '{colors.yellow.500}', _dark: '{colors.yellow.500}' },
+      value: {
+        _light: '{colors.yellow.500}',
+        _dark: '{colors.darkyellow.500}',
+      },
     },
     600: {
-      value: { _light: '{colors.yellow.600}', _dark: '{colors.yellow.600}' },
+      value: {
+        _light: '{colors.yellow.600}',
+        _dark: '{colors.darkyellow.600}',
+      },
     },
     700: {
-      value: { _light: '{colors.yellow.700}', _dark: '{colors.yellow.700}' },
+      value: {
+        _light: '{colors.yellow.700}',
+        _dark: '{colors.darkyellow.700}',
+      },
     },
     800: {
-      value: { _light: '{colors.yellow.800}', _dark: '{colors.yellow.800}' },
+      value: {
+        _light: '{colors.yellow.800}',
+        _dark: '{colors.darkyellow.800}',
+      },
     },
     900: {
-      value: { _light: '{colors.yellow.900}', _dark: '{colors.yellow.900}' },
+      value: {
+        _light: '{colors.yellow.900}',
+        _dark: '{colors.darkyellow.900}',
+      },
     },
   },
   error: {
-    25: { value: { _light: '{colors.red.25}', _dark: '{colors.red.25}' } },
-    50: { value: { _light: '{colors.red.50}', _dark: '{colors.red.50}' } },
-    100: { value: { _light: '{colors.red.100}', _dark: '{colors.red.100}' } },
-    200: { value: { _light: '{colors.red.200}', _dark: '{colors.red.200}' } },
-    300: { value: { _light: '{colors.red.300}', _dark: '{colors.red.300}' } },
-    400: { value: { _light: '{colors.red.400}', _dark: '{colors.red.400}' } },
-    500: { value: { _light: '{colors.red.500}', _dark: '{colors.red.500}' } },
-    600: { value: { _light: '{colors.red.600}', _dark: '{colors.red.600}' } },
-    700: { value: { _light: '{colors.red.700}', _dark: '{colors.red.700}' } },
-    800: { value: { _light: '{colors.red.800}', _dark: '{colors.red.800}' } },
-    900: { value: { _light: '{colors.red.900}', _dark: '{colors.red.900}' } },
+    25: { value: { _light: '{colors.red.25}', _dark: '{colors.darkred.25}' } },
+    50: { value: { _light: '{colors.red.50}', _dark: '{colors.darkred.50}' } },
+    100: {
+      value: { _light: '{colors.red.100}', _dark: '{colors.darkred.100}' },
+    },
+    200: {
+      value: { _light: '{colors.red.200}', _dark: '{colors.darkred.200}' },
+    },
+    300: {
+      value: { _light: '{colors.red.300}', _dark: '{colors.darkred.300}' },
+    },
+    400: {
+      value: { _light: '{colors.red.400}', _dark: '{colors.darkred.400}' },
+    },
+    500: {
+      value: { _light: '{colors.red.500}', _dark: '{colors.darkred.500}' },
+    },
+    600: {
+      value: { _light: '{colors.red.600}', _dark: '{colors.darkred.600}' },
+    },
+    700: {
+      value: { _light: '{colors.red.700}', _dark: '{colors.darkred.700}' },
+    },
+    800: {
+      value: { _light: '{colors.red.800}', _dark: '{colors.darkred.800}' },
+    },
+    900: {
+      value: { _light: '{colors.red.900}', _dark: '{colors.darkred.900}' },
+    },
   },
   state: {
     25: { value: { _light: '{colors.gray.25}', _dark: '{colors.gray.25}' } },
