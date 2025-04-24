b298cd63600471808e9f8c8b7d057abcdf74b178
leandro-mancini <mancinilele@gmail.com>
2025-04-24 15:54:28 -0300

    feat: 🎸 update sandbox

--- apps/sandbox-react/src/app/app.tsx
+++ apps/sandbox-react/src/app/app.tsx
@@ -1,71 +1,127 @@
-import { createSystem, defineRecipe } from 'packages/styled-system/src/index';
-import {
-  defaultConfig,
-  defaultSystem,
-  DittoProvider,
-  Stack,
-  Button,
-} from 'packages/react/src/index';
+import { Box, DittoProvider, Flex, HStack, Stack } from '@dittox/react';
 
-export const buttonRecipe = defineRecipe({
-  className: 'ditto-button',
-  base: {
-    display: 'inline-flex',
-    appearance: 'none',
-    alignItems: 'center',
-    justifyContent: 'center',
-    userSelect: 'none',
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    verticalAlign: 'middle',
-    borderWidth: '1px',
-    borderColor: 'transparent',
-    cursor: 'pointer',
-    flexShrink: '0',
-    outline: '0',
-    lineHeight: '1.2',
-    isolation: 'isolate',
-    fontWeight: 'medium',
-    transitionProperty: 'common',
-    transitionDuration: 'moderate',
-    focusVisibleRing: 'outside',
-  },
-  variants: {
-    size: {
-      xs: {},
-      sm: {},
-      md: {},
-      lg: {},
-      xl: {},
-      '2xl': {},
-    },
-    variant: {
-      primary: {},
-      secondary: {},
-      dash: {},
-      tertiary: {},
-      link: {},
-    },
-  },
-  defaultVariants: {
-    size: 'md',
-    variant: 'primary',
-  },
-});
-
-export const system = createSystem(defaultConfig, {
-  theme: {
-    recipes: {
-      button: buttonRecipe,
-    },
-  },
-});
+import { system } from './ditto.config';
+import { Button } from './ui/button/button';
 
 export function App() {
   return (
     <DittoProvider value={system}>
       <Stack gap="2rem" m="2rem">
-        <Button>Button</Button>
+        <HStack>
+          <Button colorPalette="primary" size="2xs">
+            Button
+          </Button>
+          <Button colorPalette="primary" size="xs">
+            Button
+          </Button>
+          <Button colorPalette="primary" size="sm">
+            Button
+          </Button>
+          <Button colorPalette="primary" size="md">
+            Button
+          </Button>
+          <Button colorPalette="primary" size="lg">
+            Button
+          </Button>
+          <Button colorPalette="primary" size="xl">
+            Button
+          </Button>
+          <Button colorPalette="primary" size="2xl">
+            Button
+          </Button>
+        </HStack>
+        <HStack>
+          <Button colorPalette="neutral" size="2xs">
+            Button
+          </Button>
+          <Button colorPalette="neutral" size="xs">
+            Button
+          </Button>
+          <Button colorPalette="neutral" size="sm">
+            Button
+          </Button>
+          <Button colorPalette="neutral" size="md">
+            Button
+          </Button>
+          <Button colorPalette="neutral" size="lg">
+            Button
+          </Button>
+          <Button colorPalette="neutral" size="xl">
+            Button
+          </Button>
+          <Button colorPalette="neutral" size="2xl">
+            Button
+          </Button>
+        </HStack>
+        <HStack>
+          <Button colorPalette="error" size="2xs">
+            Button
+          </Button>
+          <Button colorPalette="error" size="xs">
+            Button
+          </Button>
+          <Button colorPalette="error" size="sm">
+            Button
+          </Button>
+          <Button colorPalette="error" size="md">
+            Button
+          </Button>
+          <Button colorPalette="error" size="lg">
+            Button
+          </Button>
+          <Button colorPalette="error" size="xl">
+            Button
+          </Button>
+          <Button colorPalette="error" size="2xl">
+            Button
+          </Button>
+        </HStack>
+        <HStack>
+          <Button colorPalette="success" size="2xs">
+            Button
+          </Button>
+          <Button colorPalette="success" size="xs">
+            Button
+          </Button>
+          <Button colorPalette="success" size="sm">
+            Button
+          </Button>
+          <Button colorPalette="success" size="md">
+            Button
+          </Button>
+          <Button colorPalette="success" size="lg">
+            Button
+          </Button>
+          <Button colorPalette="success" size="xl">
+            Button
+          </Button>
+          <Button colorPalette="success" size="2xl">
+            Button
+          </Button>
+        </HStack>
+        <HStack>
+          <Button colorPalette="warning" size="2xs">
+            Button
+          </Button>
+          <Button colorPalette="warning" size="xs">
+            Button
+          </Button>
+          <Button colorPalette="warning" size="sm">
+            Button
+          </Button>
+          <Button colorPalette="warning" size="md">
+            Button
+          </Button>
+          <Button colorPalette="warning" size="lg">
+            Button
+          </Button>
+          <Button colorPalette="warning" size="xl">
+            Button
+          </Button>
+          <Button colorPalette="warning" size="2xl">
+            Button
+          </Button>
+        </HStack>
       </Stack>
     </DittoProvider>
   );
